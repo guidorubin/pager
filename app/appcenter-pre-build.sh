ccded84b14e723f56864bace489505fd5fe0b809
guido <guido.rubin@pager.com>
2019-01-15 17:47:14 -0300

    version properties

---
+++ app/appcenter-pre-build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+
+if [ ! -n "$version.properties" ]
+then
+    echo "You need define the $version.properties variable in App Center"
+    exit
+fi
+
+# This is the path to the google-services.json file, Update 'Android' to be the
+# correct path to the file relative to the root of your repo
+VERSION_PROPERTIES_FILE="version.properties"
+VERSION_CODE = "VERSION_CODE=6"
+
+if [ -e "$VERSION_PROPERTIES_FILE" ]
+then
+    echo "Updating google-services.json"
+    echo $APPCENTER_BUILD_ID > $VERSION_PROPERTIES_FILE
+
+    echo "File content:"
+    cat $VERSION_PROPERTIES_FILE
+fi
