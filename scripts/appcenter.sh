07689a011918718dae2ba35d07850a2e429ae6da
guido <guido.rubin@pager.com>
2019-01-17 19:41:14 -0300

    jq parse

--- scripts/appcenter.sh
+++ scripts/appcenter.sh
@@ -9,5 +9,4 @@ path="app/build/outputs/apk/debug/app-debug.apk"
 appcenterUrl="https://api.appcenter.ms/v0.1/apps/${appcenterAppOwner}/${appcenterAppName}/release_uploads"
 #curl -d '{"release_id": 0}' -v "data=@${path}" -H 'Content-Type: application/json' "X-API-Token: ${appcenterToken}" "${appcenterUrl}"
 response=$(curl -X POST "${appcenterUrl}" -H "accept: application/json" -H "X-API-Token: ${appcenterToken}" -H "Content-Type:application/json" -d "{ \"release_id\": 0}")
-brew install jq
 echo response | jq '.upload_url'
