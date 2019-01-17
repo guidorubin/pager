f312ed0d54afb95f91cb24ea797481db9cf57259
guido <guido.rubin@pager.com>
2019-01-17 19:18:25 -0300

    add data

--- scripts/appcenter.sh
+++ scripts/appcenter.sh
@@ -8,4 +8,4 @@ appcenterAppName=CircleApp
 path="app/build/outputs/apk/debug/app-debug.apk"
 appcenterUrl="https://api.appcenter.ms/v0.1/apps/${appcenterAppOwner}/${appcenterAppName}/release_uploads"
 #curl -d '{"release_id": 0}' -v "data=@${path}" -H 'Content-Type: application/json' "X-API-Token: ${appcenterToken}" "${appcenterUrl}"
-curl -X POST "${appcenterUrl}" -H "accept: application/json" -H "X-API-Token: ${appcenterToken}" -H "Content-Type: application/json" -d "{ \"release_id\": 0}" "data=@${path}"
+curl -X POST "${appcenterUrl}" -H "accept: application/json" -H "X-API-Token: ${appcenterToken}" -H "Content-Type:application/json" -d "{ \"release_id\": 0}" "data=@${path}"
