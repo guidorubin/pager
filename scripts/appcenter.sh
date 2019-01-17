7dda6f6689daf26640ea2e0abf7df83b1cac9398
guido <guido.rubin@pager.com>
2019-01-17 19:00:00 -0300

    try with body

--- scripts/appcenter.sh
+++ scripts/appcenter.sh
@@ -7,4 +7,4 @@ appcenterAppOwner=guido.rubin-pager.com
 appcenterAppName=CircleApp
 path="app/build/outputs/apk/debug/app-debug.apk"
 appcenterUrl="https://api.appcenter.ms/v0.1/apps/${appcenterAppOwner}/${appcenterAppName}/release_uploads"
-curl -F "data=@${path}" -H "X-API-Token: ${appcenterToken}" "${appcenterUrl}"
+curl -d '{"release_id": 0}' -v -F "status=2" -F "notifytatus=2" -F "notify=1" -F "data=@${path}" -H 'Content-Type: application/json' "X-API-Token: ${appcenterToken}" "${appcenterUrl}"
