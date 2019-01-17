507ca22143b30331dedd81e63c8ea85a6483ae3d
guido <guido.rubin@pager.com>
2019-01-17 18:45:04 -0300

    change appcenter appn

--- scripts/appcenter.sh
+++ scripts/appcenter.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 ppcenterId=${appcenter_pager_id}
 appcenterToken=${appcenter_token}
 appcenterAppOwner=guido.rubin-pager.com
-appcenterAppName=PagerApp
+appcenterAppName=CircleApp
 path="app/build/outputs/apk/debug/app-debug.apk"
 appcenterUrl="https://api.appcenter.ms/v0.1/apps/${appcenterAppOwner}/${appcenterAppName}/release_uploads"
 curl -v -F "status=2" -F "notifytatus=2" -F "notify=1" -F "ipa=@${path}" -H "X-API-Token: ${appcenterToken}" "${appcenterUrl}"
