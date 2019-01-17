6707cc28177931dcb99fb6543af06c79f61ee543
guido <guido.rubin@pager.com>
2019-01-17 18:21:42 -0300

    try appcenter upload

---
+++ scripts/appcenter.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+appcenterId=${appcenter_pager_id}
+appcenterToken=${appcenter_token}
+appcenterAppowner='guido.rubin-pager.com'
+appcenterAppName='PagerApp'
+path="app/build/outputs/apk/debug/app-debug.apk"
+appcenterUrl="https://api.appcenter.ms/v0.1/apps/${appcenterAppOwner}/${appcenterAppName}/release_uploads"
+curl -v -F "status=2" -F "notifytatus=2" -F "notify=1" -F "ipa=@${path}" -H "X-API-Token: ${appcenterToken}" "${appcenterUrl}"
