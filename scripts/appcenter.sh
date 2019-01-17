348fe2a65d3395351c8b1449e05d85caa0b32d7f
guido <guido.rubin@pager.com>
2019-01-17 19:58:58 -0300

    try to get uploadUrl

--- scripts/appcenter.sh
+++ scripts/appcenter.sh
@@ -9,6 +9,5 @@ path="app/build/outputs/apk/debug/app-debug.apk"
 appcenterUrl="https://api.appcenter.ms/v0.1/apps/${appcenterAppOwner}/${appcenterAppName}/release_uploads"
 #curl -d '{"release_id": 0}' -v "data=@${path}" -H 'Content-Type: application/json' "X-API-Token: ${appcenterToken}" "${appcenterUrl}"
 response=$(curl -X POST "${appcenterUrl}" -H "accept: application/json" -H "X-API-Token: ${appcenterToken}" -H "Content-Type:application/json" -d "{ \"release_id\": 0}")
-echo ${response}
-uploadUrl=${response} | jq -r '.upload_url'
+uploadUrl=(${response} | jq -r '.upload_url')
 echo ${uploadUrl}
