a1eee70d41227b411820c1425e9de93657598db3
guido <guido.rubin@pager.com>
2019-01-18 11:48:02 -0300

    &&

--- scripts/appcenter.sh
+++ scripts/appcenter.sh
@@ -18,7 +18,6 @@ appcenterUrl="https://api.appcenter.ms/v0.1/apps/${appcenterAppOwner}/${appcente
 #curl -X POST "${appcenterUrl}" -H "accept: application/json" -H "X-API-Token: ${appcenterToken}" -H "Content-Type:application/json" -d "{ \"release_id\": 0}" | jq -r '.upload_id'
 
 
-curl -v -F "ipa=@${path}" $(curl -X POST "${appcenterUrl}" -H "accept: application/json" -H "X-API-Token: ${appcenterToken}" -H "Content-Type:application/json" -d "{ \"release_id\": 0}" | jq -r '.upload_url')
+curl -v -F "ipa=@${path}" $(curl -X POST "${appcenterUrl}" -H "accept: application/json" -H "X-API-Token: ${appcenterToken}" -H "Content-Type:application/json" -d "{ \"release_id\": 0}" | jq -r '.upload_url') &&
 
-sleep 300
 curl -v -X PATCH --header 'Content-Type: application/json' --header 'Accept: application/json' --header "X-API-Token: ${appcenterToken}" -d '{ "status": "committed"  }' "${appcenterUrl}/$(curl -X POST "${appcenterUrl}" -H "accept: application/json" -H "X-API-Token: ${appcenterToken}" -H "Content-Type:application/json" -d "{ \"release_id\": 0}" | jq -r '.upload_id')"
