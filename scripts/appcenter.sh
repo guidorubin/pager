fae77de31dfb5e1c9f7fcd59dfd741ae0c943098
guido <guido.rubin@pager.com>
2019-01-18 10:50:58 -0300

    add jq to curl

--- scripts/appcenter.sh
+++ scripts/appcenter.sh
@@ -8,7 +8,7 @@ appcenterAppName=CircleApp
 path="app/build/outputs/apk/debug/app-debug.apk"
 appcenterUrl="https://api.appcenter.ms/v0.1/apps/${appcenterAppOwner}/${appcenterAppName}/release_uploads"
 #curl -d '{"release_id": 0}' -v "data=@${path}" -H 'Content-Type: application/json' "X-API-Token: ${appcenterToken}" "${appcenterUrl}"
-response=$(curl -X POST "${appcenterUrl}" -H "accept: application/json" -H "X-API-Token: ${appcenterToken}" -H "Content-Type:application/json" -d "{ \"release_id\": 0}")
+response=$(curl -X POST "${appcenterUrl}" -H "accept: application/json" -H "X-API-Token: ${appcenterToken}" -H "Content-Type:application/json" -d "{ \"release_id\": 0}") | jq '.upload_url'
 #uploadUrl=$(jq -r '.upload_url' ${response})
 
-cat "${response}" | jq '.upload_url'
+echo ${response}
