c039d9f44dfdbbd5db734fafaa06b3033e1c277e
Guido <guidonrubin@gmail.com>
2019-01-18 12:26:02 -0300

    Circle branch (#1)
    * circle tests
    
    * change gradlew dependencie
    
    * change ./
    
    * change gradlew to gradle
    
    * working directory?
    
    * try again with gradlew
    
    * permissons
    
    * assemble debug
    
    * assemble debug
    
    * change save cache order
    
    * adnroid api missing
    
    * save artifacts
    
    * change directory
    
    * try appcenter upload
    
    * error on yaml
    
    * wrong indent
    
    * try appcenter
    
    * chmod
    
    * chmod
    
    * wrong bash
    
    * change appcenter appn
    
    * try with data
    
    * try with body
    
    * try with body
    
    * change to post
    
    * add data
    
    * add data
    
    * jq parse
    
    * jq parse
    
    * jq parse
    
    * jq parse
    
    * jq parse
    
    * try to get uploadUrl
    
    * try to get uploadUrl
    
    * try to get uploadUrl
    
    * try to get uploadUrl
    
    * try to get uploadUrl 2
    
    * try to get uploadUrl 2
    
    * try with cat
    
    * try with cat
    
    * add jq to curl
    
    * add jq to curl
    
    * add jq to curl
    
    * upload step
    
    * last step
    
    * last step
    
    * last step
    
    * verbose
    
    * sleep
    
    * &&
    
    * -F
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow

---
+++ scripts/appcenter.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ppcenterId=${appcenter_pager_id}
+appcenterToken=${appcenter_token}
+appcenterAppOwner=guido.rubin-pager.com
+appcenterAppName=CircleApp
+path="app/build/outputs/apk/debug/app-debug.apk"
+appcenterUrl="https://api.appcenter.ms/v0.1/apps/${appcenterAppOwner}/${appcenterAppName}/release_uploads"
+#curl -d '{"release_id": 0}' -v "data=@${path}" -H 'Content-Type: application/json' "X-API-Token: ${appcenterToken}" "${appcenterUrl}"
+
+
+#response=$(curl -X POST "${appcenterUrl}" -H "accept: application/json" -H "X-API-Token: ${appcenterToken}" -H "Content-Type:application/json" -d "{ \"release_id\": 0}" | jq -r '.upload_url')
+#uploadUrl=$(jq -r '.upload_url' ${response})
+
+
+
+#curl -X POST "${appcenterUrl}" -H "accept: application/json" -H "X-API-Token: ${appcenterToken}" -H "Content-Type:application/json" -d "{ \"release_id\": 0}" | jq -r '.upload_id'
+
+
+curl -v -F "status=2" -F "notifytatus=2" -F "notify=1" -F "ipa=@${path}" $(curl -X POST "${appcenterUrl}" -H "accept: application/json" -H "X-API-Token: ${appcenterToken}" -H "Content-Type:application/json" -d "{ \"release_id\": 0}" | jq -r '.upload_url') &&
+
+curl -v -X PATCH --header 'Content-Type: application/json' --header 'Accept: application/json' --header "X-API-Token: ${appcenterToken}" -d '{ "status": "committed"  }' "${appcenterUrl}/$(curl -X POST "${appcenterUrl}" -H "accept: application/json" -H "X-API-Token: ${appcenterToken}" -H "Content-Type:application/json" -d "{ \"release_id\": 0}" | jq -r '.upload_id')"
