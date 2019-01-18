86dcf0aa5a1db83c9c4b66e090bb9b7d285384ee
guido <guido.rubin@pager.com>
2019-01-18 10:44:28 -0300

    try with cat

--- scripts/appcenter.sh
+++ scripts/appcenter.sh
@@ -9,6 +9,6 @@ path="app/build/outputs/apk/debug/app-debug.apk"
 appcenterUrl="https://api.appcenter.ms/v0.1/apps/${appcenterAppOwner}/${appcenterAppName}/release_uploads"
 #curl -d '{"release_id": 0}' -v "data=@${path}" -H 'Content-Type: application/json' "X-API-Token: ${appcenterToken}" "${appcenterUrl}"
 response=$(curl -X POST "${appcenterUrl}" -H "accept: application/json" -H "X-API-Token: ${appcenterToken}" -H "Content-Type:application/json" -d "{ \"release_id\": 0}")
-uploadUrl=$(jq -r '.upload_url' ${response})
+#uploadUrl=$(jq -r '.upload_url' ${response})
 
-echo ${uploadUrl}
+cat ${response} | jq '.upload_url'
